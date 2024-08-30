c812972e970f51ed9b35eab4929443585ce32c09
Shobhit Singh <majin.shobhitrawat53@gmail.com>
2024-08-30 20:45:54 +0530

    Ui is done

--- app/src/main/java/com/example/physicswalla_assignment/ui/homePackage/CharacterListScreen.kt
+++ app/src/main/java/com/example/physicswalla_assignment/ui/homePackage/CharacterListScreen.kt
@@ -2,13 +2,26 @@ package com.example.physicswalla_assignment.ui.homePackage
 
 
 import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.physicswalla_assignment.R
 import com.example.physicswalla_assignment.models.CharacterApiResponseModel
 import com.example.physicswalla_assignment.models.Result
 import com.example.physicswalla_assignment.ui.widgets.ErrorPopup
@@ -18,8 +31,7 @@ import com.example.physicswalla_assignment.utils.NetworkResult
 
 
 @Composable
-
-fun CharacterListScreen(navController:NavController,viewModel: CharacterViewModel) {
+fun CharacterListScreen(navController: NavController, viewModel: CharacterViewModel) {
 
     val characterListState by viewModel.getCharacterList.observeAsState()
 
@@ -27,33 +39,43 @@ fun CharacterListScreen(navController:NavController,viewModel: CharacterViewMode
         viewModel.getAllCharacter()
     }
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Rick and Morty Characters",
 
-    when(characterListState){
-        is NetworkResult.Error -> {
+            style = MaterialTheme.typography.headlineMedium.copy(
+                fontFamily = FontFamily(Font(R.font.poppins_regular)) // Use Poppins font
+            ),
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(bottom = 14.dp)
+        )
 
-            Log.d("checking",characterListState?.message.toString())
-            val errorMessage = (characterListState as NetworkResult.Error).message
-            ErrorPopup(message = errorMessage ?: "An unknown error occurred")
-        }
-        is NetworkResult.Loading -> {
-//            ProgressBar()
-        }
-        is NetworkResult.Success -> {
-
-            Log.d("checking",characterListState?.data.toString())
-
-            val characterList = (characterListState as NetworkResult.Success<CharacterApiResponseModel>).data?.results
-            if (characterList != null) {
-                LayoutCharacterList(characterList,navController)
-
+        when (characterListState) {
+            is NetworkResult.Error -> {
+                Log.d("checking", characterListState?.message.toString())
+                val errorMessage = (characterListState as NetworkResult.Error).message
+                ErrorPopup(message = errorMessage ?: "An unknown error occurred")
             }
+            is NetworkResult.Loading -> {
+                // ProgressBar()
+            }
+            is NetworkResult.Success -> {
+                Log.d("checking", characterListState?.data.toString())
+                val characterList = (characterListState as NetworkResult.Success<CharacterApiResponseModel>).data?.results
+                if (characterList != null) {
+                    LayoutCharacterList(characterList, navController)
+                }
+            }
+            null -> {}
         }
-        null -> {}
     }
-
-
-
 }
+
 
 @Composable
 fun LayoutCharacterList(characterList: List<Result>, navController: NavController) {
--- app/src/main/java/com/example/physicswalla_assignment/ui/widgets/LayoutCharacterItem.kt
+++ app/src/main/java/com/example/physicswalla_assignment/ui/widgets/LayoutCharacterItem.kt
@@ -1,52 +1,76 @@
 package com.example.physicswalla_assignment.ui.widgets
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
+import com.example.physicswalla_assignment.R
 import com.example.physicswalla_assignment.models.Result
-
+import com.example.physicswalla_assignment.ui.theme.poppinsFontFamily
 @Composable
-fun  LayoutCharacterItem(character : Result , onClick :() -> Unit) {
-    
-    Row (modifier = Modifier
-        .padding(8.dp)
-        .fillMaxWidth()
-        .clickable { onClick() }){
+fun LayoutCharacterItem(character: Result, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .clickable { onClick() },
 
-        Image(
-            painter = rememberAsyncImagePainter(character.image),
-            contentDescription = character.name,
-            modifier = Modifier
-                .size(80.dp)
-                .padding(8.dp),
-            contentScale = ContentScale.Crop
-        )
-        Column(
+        shape = RoundedCornerShape(8.dp),
+
+    ) {
+        Row(
             modifier = Modifier
                 .padding(8.dp)
-                .fillMaxHeight()
-        ){
-            Text(text = character.name, fontSize = 20.sp)
-            Text(text = "Species: ${character.species}", fontSize = 16.sp)
-            Text(text = "Gender: ${character.gender}", fontSize = 16.sp)
-            Text(text = "Status: ${character.status}", fontSize = 16.sp)
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min) // Adjust the height based on the content
+        ) {
+            Image(
+                painter = rememberAsyncImagePainter(character.image),
+                contentDescription = character.name,
+                modifier = Modifier
+                    .size(80.dp)
+                    .padding(8.dp)
+                    .align(Alignment.CenterVertically), // Center the image vertically
+                contentScale = ContentScale.Crop
+            )
+
+            Column(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxHeight(),
+                verticalArrangement = Arrangement.Center // Center the text vertically in the column
+            ) {
+                Text(text = character.name, fontSize = 20.sp, fontFamily = FontFamily(Font(R.font.poppins_regular)))
+                Text(text = "Species: ${character.species}", fontSize = 16.sp, fontFamily = FontFamily(Font(R.font.poppins_regular)))
+                Text(text = "Gender: ${character.gender}", fontSize = 16.sp, fontFamily = FontFamily(Font(R.font.poppins_regular)))
+                Text(text = "Status: ${character.status}", fontSize = 16.sp, fontFamily = FontFamily(Font(R.font.poppins_regular)))
+            }
         }
-
-
-
-
     }
-    
 }
